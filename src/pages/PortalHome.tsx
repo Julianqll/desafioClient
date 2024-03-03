221ac0a1af7460e30bdc4c83563d3f133c21440b
Julian Quispe <julianmql@hotmail.com>
2024-03-03 03:53:37 -0500

    feat: resources table added

--- src/pages/PortalHome.tsx
+++ src/pages/PortalHome.tsx
@@ -1,9 +1,8 @@
 import { CollapseDesktop } from "../components/CollapseDesktop/CollapseDesktop";
+import { ResourcesTable } from "../components/ResourcesTable/ResourcesTable";
 
 export function PortalHome() {
     return <CollapseDesktop>
-    <div>
-        Hola
-    </div>
+    <ResourcesTable></ResourcesTable>
     </CollapseDesktop>
 }
