8089c44a88342f0316a8d1ef07cf8cb5e54e56e2
Julian Quispe <julianmql@hotmail.com>
2024-03-03 18:44:13 -0500

    fix: resources table fixed

--- src/components/ResourcesTable/ResourcesTable.tsx
+++ src/components/ResourcesTable/ResourcesTable.tsx
@@ -54,36 +54,17 @@ export function ResourcesTable() {
   const rows = data.map((item) => (
     <Table.Tr key={item.name}>
       <Table.Td>
-        <Group gap="sm">
-          <Avatar size={30} src={item.avatar} radius={30} />
-          <Text fz="sm" fw={500}>
-            {item.name}
-          </Text>
-        </Group>
-      </Table.Td>
-
-      <Table.Td>
-        <Badge color={jobColors[item.job.toLowerCase()]} variant="light">
-          {item.job}
-        </Badge>
+        <Text fz="sm" fw={500}>
+          {item.name}
+        </Text>
       </Table.Td>
       <Table.Td>
-        <Anchor component="button" size="sm">
-          {item.email}
-        </Anchor>
+        <Text fz="sm" fw={500}>
+          {item.name}
+        </Text>
       </Table.Td>
       <Table.Td>
         <Text fz="sm">{item.phone}</Text>
-      </Table.Td>
-      <Table.Td>
-        <Group gap={0} justify="flex-end">
-          <ActionIcon variant="subtle" color="gray">
-            <IconPencil style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon variant="subtle" color="red">
-            <IconTrash style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-          </ActionIcon>
-        </Group>
       </Table.Td>
     </Table.Tr>
   ));
@@ -93,11 +74,9 @@ export function ResourcesTable() {
       <Table verticalSpacing="sm">
         <Table.Thead>
           <Table.Tr>
-            <Table.Th>Employee</Table.Th>
-            <Table.Th>Job title</Table.Th>
-            <Table.Th>Email</Table.Th>
-            <Table.Th>Phone</Table.Th>
-            <Table.Th />
+            <Table.Th>Nombre</Table.Th>
+            <Table.Th>Direccion</Table.Th>
+            <Table.Th>Contacto</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
